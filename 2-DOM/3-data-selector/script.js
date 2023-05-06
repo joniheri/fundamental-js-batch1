64f1a87259a4854a06c57436edc2674f57ac220b
jonheri <jonheri001@gmail.com>
2023-05-06 09:38:40 +0700

    DOM Data Selector

---
+++ 2-DOM/3-data-selector/script.js
@@ -0,0 +1,7 @@
+const tagh2 = document.getElementById("taghDua");
+console.log(tagh2);
+tagh2.innerHTML = "Ini tag h2 sudah di edit";
+
+const tagh3 = document.querySelector(".taghTiga");
+console.log(tagh2);
+tagh3.innerHTML = "Ini tag h3 sudah di edit";
